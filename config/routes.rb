afcb9ce6effdbc413154de048636ee4351a3a848
ryu makiguchi <makiguchir@gmail.com>
2023-01-18 17:20:23 -0300

    routes fixes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "horses#index"
-  resources :horses
+  root to: "horses#index"
+  resources :horses, except: :index
   resources :auctions
 end
